66c8c8ce4ccbb8cf09d13d1fc7b615491e916d89
kan kawabata <kkawabat@asu.edu>
2023-07-31 15:46:01 -0700

    tweeked duration threshold and minor update to readme.md x2

--- README.md
+++ README.md
@@ -2,16 +2,23 @@
 
 This is the business logic for my portfolio project on kankawabata.com/morse_code. Some of the code here were lifted out of my previous kivy implementation see [here](https://github.com/kkawabat/morse-code-kivy)
 
-## signal encoding
-The Morse Engine "signal" is a sequence of number and alternating "U"s and "D"s representing key not pressed state and keydown state respectively. The number represents the duration of the states in 10ms. 
+## usage
+The `MorseEngine` class takes one parameter `tapping_speed`. tapping_speed determines the duration that the user needs to press to register as a dot/dash/short pause/long pause. The smaller the number the quicker the user will have to tap.
 
-So for example the signal:
+
+
+## signal encoding
+The Morse Engine "signal" is a sequence of number and alternating "U"s and "D"s, such as the one seen below:
 
 ```
 23U1D1U1D1U1D1U1D
 ```
 
-represents 230ms (23 * 10ms) of key not pressed, followed by 10ms of key pressed followed by 10ms of key released, etc.
+There are two states when transmitting morse code the pressed and unpressed states. the number in the signal represents the number of frame (10ms increments) of the state and the U/D following after indicates the state (unpressed/pressed respectively)  
+
+So for example in the above signal `23U` represents 23 frames (230ms) of the key not being pressed followed by 1 frame of the key being pressed (`1d`), etc.
+
+
 
 
 
--- src/morse_code_util/morse_engine.py
+++ src/morse_code_util/morse_engine.py
@@ -6,32 +6,34 @@ CHAR_TO_MORSE_MAP = {'a': '.-', 'b': '-...', 'c': '-.-.', 'd': '-..', 'e': '.', 
                      '6': '-....', '7': '--...', '8': '---..', '9': '----.', ' ': '/'}
 MORSE_TO_CHAR_MAP = {v: k for k, v in CHAR_TO_MORSE_MAP.items()}
 
+SIGNAL_FRAME_PERIOD = 10  # 10 ms
+
 
 class MorseEngine:
-    short_duration_threshold = 8  # signal held for <80ms is considered a "dot"
-    short_pause_duration_threshold = 8  # signal pause for <80ms is considered a new code
-    long_pause_duration_threshold = 24  # signal pause for <240ms is considered a short pause else it's a long pause
+    def __init__(self, tapping_speed=.08):
+        self.tapping_speed = tapping_speed
+        self.short_duration_threshold = tapping_speed * SIGNAL_FRAME_PERIOD
+        self.short_pause_duration_threshold = tapping_speed * SIGNAL_FRAME_PERIOD * 3
+        self.long_pause_duration_threshold = tapping_speed * SIGNAL_FRAME_PERIOD * 7
 
-    @classmethod
-    def signal_to_text(cls, signal):
-        morse = cls.signal_to_morse(signal)
-        text = cls.morse_to_text(morse)
+    def signal_to_text(self, signal):
+        morse = self.signal_to_morse(signal)
+        text = self.morse_to_text(morse)
         return text
 
-    @classmethod
-    def signal_to_morse(cls, signal):
+    def signal_to_morse(self, signal):
         morse = [dur for dur in signal.replace('U', 'D').split('D') if dur != '']
         up = []
         for duration in morse[::2]:
             duration = int(duration)
-            if duration <= cls.short_pause_duration_threshold:
+            if duration <= self.short_pause_duration_threshold:
                 up.append('')
-            elif int(duration) <= cls.long_pause_duration_threshold:
+            elif int(duration) <= self.long_pause_duration_threshold:
                 up.append(' ')
             else:
                 up.append(' / ')
 
-        down = ['.' if int(duration) <= cls.short_duration_threshold else '-' for duration in morse[1::2]]
+        down = ['.' if int(duration) <= self.short_duration_threshold else '-' for duration in morse[1::2]]
         morse = ''.join([x for y in zip(up, down) for x in y])
         return morse
 
@@ -48,15 +50,16 @@ class MorseEngine:
 
 
 if __name__ == '__main__':
+    me = MorseEngine()
     sample_signal = '23U1D1U1D1U1D1U1D12U1D7U1D2U7D2U1D11U2D7U1D2U7D2U1D1U1D11U6D1U6D1U6D20U'
 
-    sample_morse = MorseEngine.signal_to_morse(sample_signal)
+    sample_morse = me.signal_to_morse(sample_signal)
     print(sample_morse)
 
-    sample_text = MorseEngine.morse_to_text(sample_morse)
+    sample_text = me.morse_to_text(sample_morse)
     print(sample_text)
 
-    sample_reversed_morse = MorseEngine.text_to_morse(sample_text)
+    sample_reversed_morse = me.text_to_morse(sample_text)
     print(sample_reversed_morse)
 
-    print(MorseEngine.text_to_morse('hello world'))
+    print(me.text_to_morse('hello world'))
